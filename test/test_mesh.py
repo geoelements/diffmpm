41bcc4a8227b7144250821bf05163a821fd65415
Chahak Mehta <chahakmehta013@gmail.com>
2023-03-09 22:15:37 -0600

    Add function to update particles' natural coords

--- test/test_mesh.py
+++ test/test_mesh.py
@@ -40,3 +40,33 @@ def test_particle_element_mapping_update():
     assert (
         mesh.particles.element_ids == jnp.array([-1, -1, -1, -1, -1, -1])
     ).all()
+
+
+def test_particle_xi_update():
+    material = Material(1, 1)
+    mesh = Mesh1D(2, material, 2, ppe=3)
+    mesh._update_particle_natural_coords()
+    assert jnp.allclose(
+        mesh.particles.xi, jnp.tile(jnp.array([-2 / 3, 0, 2 / 3]), 2)
+    )
+    mesh.particles.x += 1
+    mesh._update_particle_element_ids()
+    mesh._update_particle_natural_coords()
+    assert jnp.allclose(
+        mesh.particles.xi, jnp.array([-2 / 3, 0, 2 / 3, 0, 0, 0])
+    )
+    mesh.particles.x -= 2
+    mesh._update_particle_element_ids()
+    mesh._update_particle_natural_coords()
+    assert jnp.allclose(
+        mesh.particles.xi, jnp.array([0, 0, 0, -2 / 3, 0, 2 / 3])
+    )
+
+
+def test_element_node_mapping():
+    material = Material(1, 1)
+    mesh = Mesh1D(2, material, 2, ppe=3)
+    assert (
+        mesh._get_element_node_pos(0)
+        == mesh.nodes.position[jnp.asarray([0, 1])]
+    ).all()
