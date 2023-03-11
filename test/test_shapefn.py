3e3c85d95835083f9a51509f7ff10922f0c91373
Chahak Mehta <chahakmehta013@gmail.com>
2023-03-10 18:01:53 -0600

    Correct shapefn dims and add vector tests

--- test/test_shapefn.py
+++ test/test_shapefn.py
@@ -16,3 +16,19 @@ def test_shapefn_1d_scalar_grad():
     sf = ShapeFn(dim=1)
     true_grad = jnp.array([-0.5, 0.5])
     assert jnp.all(sf.shapefn_grad(x, jnp.array([-1, 1])) == true_grad)
+
+
+def test_shapefn_1d_vec():
+    x = jnp.array([0.5, 0.5])
+    sf = ShapeFn(dim=1)
+    true_sf = jnp.array([[0.25, 0.75], [0.25, 0.75]])
+    calc_sf = sf.shapefn(x)
+    assert jnp.allclose(calc_sf, true_sf)
+
+
+def test_shapefn_1d_vec_grad():
+    x = jnp.array([0.5, 0.5])
+    sf = ShapeFn(dim=1)
+    true_grad = jnp.array([[-0.5, 0.5], [-0.5, 0.5]])
+    calc_grad = sf.shapefn_grad(x, jnp.array([-1, 1]))
+    assert jnp.allclose(calc_grad, true_grad)
