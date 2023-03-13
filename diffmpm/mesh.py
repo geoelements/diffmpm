aad1dc8c53377e73a2de565775b6e824702a1b07
Chahak Mehta <chahakmehta013@gmail.com>
2023-03-13 13:56:32 -0500

    Add new update functions and tests

--- diffmpm/mesh.py
+++ diffmpm/mesh.py
@@ -79,6 +79,13 @@ class Mesh1D:
                 f"yet supported. Please use 'uniform'."
             )
 
+    def _get_element_node_ids(self, element_idx):
+        """
+        Given an element at index `element_idx`, return the
+        mapping node coordinates for that element.
+        """
+        return jnp.asarray([element_idx, element_idx + 1])
+
     def _get_element_node_pos(self, element_idx):
         """
         Given an element at index `element_idx`, return the
@@ -91,7 +98,7 @@ class Mesh1D:
         Given an element at index `element_idx`, return the
         mapping node coordinates for that element.
         """
-        return self.nodes.velocity[element_idx, element_idx + 1]
+        return self.nodes.velocity[jnp.asarray([element_idx, element_idx + 1])]
 
     def _update_particle_element_ids(self):
         """
@@ -131,7 +138,7 @@ class Mesh1D:
         the natural coordinates of the particles based on the element
         a particle is a part of. The update formula is
 
-        :math:`\xi = \frac{(x - x_{n_0}) 2}{l} - 1`
+        :math:`xi = (x - x_{n_0}) 2 / l - 1`
 
         If a particle is not in any element (element_id = -1), its
         natural coordinate is set to 0.
@@ -145,12 +152,17 @@ class Mesh1D:
         xi_coords = (self.particles.x - t) * 2 / self.element_length - 1
         self.particles.xi = xi_coords
 
-    def _update_particle_strain(self):
+    def _update_particle_strain(self, dt):
         """
         Calculate the strain values for particles.
 
         This calculation is done by mapping the nodal velocities
         with the gradient of the interpolation shape function.
+
+        Arguments
+        ---------
+        dt : float
+            Time step.
         """
 
         nodal_coords = vmap(self._get_element_node_pos)(
@@ -163,12 +175,133 @@ class Mesh1D:
         nodal_vel = vmap(self._get_element_node_vel)(self.particles.element_ids)
 
         # strain rate is the row-wise sum of the matrix particles_dndx x nodal_vel
-        strain_rate = jnp.sum(particles_dndx * nodal_vel, axis=0)
+        strain_rate = jnp.sum(particles_dndx * nodal_vel, axis=1)
 
         self.particles.dstrain = strain_rate * dt
         self.particles.strain += self.particles.dstrain
         return
 
     def _update_particle_stress(self):
-        self.particle.stress += self.particle.dstrain * self.material.E
+        self.particles.stress += self.particles.dstrain * self.material.E
         return
+
+    def _update_nodes_acc_vel(self, dt):
+        """
+        Compute acceleration based velocity.
+
+        The velocity at nodes is calculated based on the acceleration
+        achieved by the force on the nodes and added to the current
+        velocity. For velocity update using momentum see
+        `diffmpm.Mesh._update_nodes_mom_vel()`
+
+        Arguments
+        ---------
+        dt : float
+            Time step.
+        """
+        total_force = self.nodes.f_int + self.nodes.f_ext + self.nodes.f_damp
+
+        def f(f, m):
+            nodal_acceleration = lax.cond(
+                m == 0,
+                lambda cf, cm: 0.0,
+                lambda cf, cm: jnp.divide(cf, cm),
+                f,
+                m,
+            )
+            return nodal_acceleration
+
+        nodal_acceleration = vmap(f)(total_force, self.nodes.mass)
+        self.nodes.velocity += nodal_acceleration * dt
+
+    def _update_nodes_mom_vel(self):
+        """
+        Compute momentum based velocity.
+
+        The velocity of the nodes is calculated based on the current
+        momentum at the nodes. This function _sets_ the value of the
+        velocities for each node. For acceleration based update see
+        `diffmpm.Mesh._update_nodes_acc_vel()`
+        """
+
+        def f(p, m):
+            velocity = lax.cond(
+                m == 0,
+                lambda cp, cm: jnp.zeros_like(cp),
+                lambda cp, cm: jnp.divide(cp, cm),
+                p,
+                m,
+            )
+            return velocity
+
+        nodal_velocity = vmap(f)(self.nodes.momentum, self.nodes.mass)
+        self.nodes.velocity = nodal_velocity
+
+    def _update_nodes_bc_mom_vel(self):
+        """
+        Set momentum and velocity of boundary nodes.
+
+        Based on the boundary conditions of the mesh, the nodes at the
+        boundary points are set to 0 momentum and velocity.
+        """
+        self.nodes.momentum = self.nodes.momentum.at[self.boundary_nodes].set(0)
+        self.nodes.velocity = self.nodes.velocity.at[self.boundary_nodes].set(0)
+
+    def _update_nodes_bc_force(self):
+        """
+        Set forces of boundary nodes.
+
+        Based on the boundary conditions of the mesh, the forces on the
+        nodes at the boundary points are set to 0.
+        """
+        self.nodes.f_int = self.nodes.f_int.at[self.boundary_nodes].set(0)
+        self.nodes.f_ext = self.nodes.f_ext.at[self.boundary_nodes].set(0)
+        self.nodes.f_damp = self.nodes.f_damp.at[self.boundary_nodes].set(0)
+
+    def _update_node_momentum_force(self, dt):
+        """
+        Update the momentum at nodes based on force
+
+        :math:`p += total_force * dt`
+
+        Arguments
+        ---------
+        dt : float
+            Time step.
+        """
+        self.nodes.momentum += (
+            self.nodes.f_int + self.nodes.f_ext + self.nodes.f_damp
+        ) * dt
+
+    def _update_node_momentum_par_vel(self):
+        """
+        Update the nodal momentum based on particle velocity.
+
+        The nodal momentum is updated as a sum of particle momentum for
+        all particles mapped to the node.
+
+        :math:`(mv)_i = \sum_p N_i(x_p) m_p v_p`
+        """
+        self.nodes.momentum = self.nodes.momentum.at[:].set(0)
+
+        def step(pid, args):
+            element_ids, momentum, mass, velocity, mapped_pos, el_nodes = args
+            eid = element_ids[pid]
+            momentum = momentum.at[el_nodes[pid]].add(
+                mass[pid] * velocity[pid] * mapped_pos[pid]
+            )
+            return element_ids, momentum, mass, velocity, mapped_pos, el_nodes
+
+        mapped_positions = self.shapefn.shapefn(self.particles.x)
+        mapped_nodes = vmap(self._get_element_node_ids)(
+            self.particles.element_ids
+        )
+        args = (
+            self.particles.element_ids,
+            self.nodes.momentum,
+            self.particles.mass,
+            self.particles.velocity,
+            mapped_positions,
+            mapped_nodes,
+        )
+        _, self.nodes.momentum, _, _, _, _ = lax.fori_loop(0, len(self.particles), step, args)
--- test/test_mesh.py
+++ test/test_mesh.py
@@ -72,3 +72,79 @@ def test_element_node_mapping():
         mesh._get_element_node_pos(0)
         == mesh.nodes.position[jnp.asarray([0, 1])]
     ).all()
+
+
+def test_particle_strain_stress_update():
+    material = Material(2, 1)
+    mesh = Mesh1D(2, material, 2, jnp.array([0]), ppe=2)
+    mesh.nodes.velocity = jnp.array([0, 1, 1])
+    mesh._update_particle_strain(0.1)
+    mesh._update_particle_stress()
+    # Actual solution:
+    # particles.x = [0.25, 0.75, 1.25, 1.75]
+    # nodes.v = [0, 1, 1]
+    # shapefn_grad = [[-1, 1], [-1, 1], [-1, 1], [-1, 1]]
+    # strain_rate[i] = shapefn_grad[i][0] * nodes.v[0] + shapefn_grad[i][1] * nodes.v[1]
+    # strain[i] = strain_rate[i] * dt
+    assert jnp.allclose(mesh.particles.strain, jnp.array([0.1, 0.1, 0, 0]))
+    assert jnp.allclose(mesh.particles.stress, jnp.array([0.2, 0.2, 0, 0]))
+
+
+def test_nodes_acc_velocity():
+    material = Material(2, 1)
+    mesh = Mesh1D(2, material, 2, jnp.array([0]), ppe=2)
+    mesh.nodes.mass = jnp.ones_like(mesh.nodes.position) * 2
+    mesh.nodes.f_int = jnp.ones_like(mesh.nodes.position)
+    mesh.nodes.f_ext = jnp.ones_like(mesh.nodes.position)
+    mesh._update_nodes_acc_vel(0.1)
+    assert jnp.allclose(mesh.nodes.velocity, jnp.array([0.1, 0.1, 0.1]))
+
+    # This shouldn't change velocity since mass is 0
+    mesh.nodes.mass = jnp.zeros_like(mesh.nodes.position)
+    mesh._update_nodes_acc_vel(0.1)
+    assert jnp.allclose(mesh.nodes.velocity, jnp.array([0.1, 0.1, 0.1]))
+
+
+def test_nodes_mom_velocity():
+    material = Material(2, 1)
+    mesh = Mesh1D(2, material, 2, jnp.array([0]), ppe=2)
+    mesh.nodes.mass = jnp.ones_like(mesh.nodes.position) * 2
+    mesh.nodes.momentum = jnp.ones_like(mesh.nodes.position)
+    mesh._update_nodes_mom_vel()
+    assert jnp.allclose(mesh.nodes.velocity, jnp.array([0.5, 0.5, 0.5]))
+
+    # This should velocity to 0 since mass is 0
+    mesh.nodes.mass = jnp.zeros_like(mesh.nodes.position)
+    mesh._update_nodes_mom_vel()
+    assert jnp.allclose(mesh.nodes.velocity, jnp.array([0.0, 0.0, 0.0]))
+
+
+def test_nodes_bc_force_update():
+    material = Material(2, 1)
+    mesh = Mesh1D(2, material, 2, jnp.array([0]), ppe=2)
+    mesh.nodes.f_int = jnp.ones_like(mesh.nodes.position)
+    mesh.nodes.f_ext = jnp.ones_like(mesh.nodes.position)
+    mesh.nodes.f_damp = jnp.ones_like(mesh.nodes.position)
+    mesh._update_nodes_bc_force()
+    assert jnp.all(mesh.nodes.f_int == jnp.array([0, 1, 1]))
+    assert jnp.all(mesh.nodes.f_ext == jnp.array([0, 1, 1]))
+    assert jnp.all(mesh.nodes.f_damp == jnp.array([0, 1, 1]))
+
+
+def test_nodes_bc_mom_vel_update():
+    material = Material(2, 1)
+    mesh = Mesh1D(2, material, 2, jnp.array([0]), ppe=2)
+    mesh.nodes.mass = jnp.ones_like(mesh.nodes.position) * 2
+    mesh.nodes.momentum = jnp.ones_like(mesh.nodes.position)
+    mesh.nodes.velocity = jnp.ones_like(mesh.nodes.position)
+    mesh._update_nodes_bc_mom_vel()
+    assert jnp.all(mesh.nodes.momentum == jnp.array([0, 1, 1]))
+    assert jnp.all(mesh.nodes.momentum == jnp.array([0, 1, 1]))
+
+
+def test_nodes_momentum_vel_update():
+    material = Material(2, 1)
+    mesh = Mesh1D(2, material, 2, jnp.array([0]), ppe=2)
+    mesh.particles.velocity = jnp.ones_like(mesh.particles.x)
+    mesh._update_node_momentum_par_vel()
+    assert jnp.allclose(mesh.nodes.momentum, jnp.array([0.25, 0.5, 1.25]))
