ad7c544326e9d5f6dc171bd3193e8f7d3dff9f89
Chahak Mehta <chahakmehta013@gmail.com>
2023-05-12 15:42:55 -0500

    Refactor Material class

--- diffmpm/material.py
+++ diffmpm/material.py
@@ -1,33 +1,59 @@
 from jax.tree_util import register_pytree_node_class
+import abc
 
 
-@register_pytree_node_class
-class Material:
-    """
-    Base material class.
-    """
+class Material(abc.ABC):
+    """Base material class."""
 
-    def __init__(self, E, density):
+    def __init__(self, material_properties):
         """
         Initialize material properties.
 
         Arguments
         ---------
-        E : float
-            Young's modulus of the material.
-        density : float
-            Density of the material.
+        material_properties: dict
+            A key-value map for various material properties.
         """
-        self.E = E
-        self.density = density
+        self.material_properties = material_properties
 
+    @abc.abstractmethod
     def tree_flatten(self):
-        return (tuple(), (self.E, self.density))
+        """Flatten this class as PyTree Node."""
+        ...
 
     @classmethod
+    @abc.abstractmethod
     def tree_unflatten(cls, aux_data, children):
-        del children
-        return cls(*aux_data)
+        """Unflatten this class as PyTree Node."""
+        ...
 
+    @abc.abstractmethod
     def __repr__(self):
-        return f"Material(E={self.E}, density={self.density})"
+        """Repr for Material class."""
+        ...
+
+
+@register_pytree_node_class
+class LinearElastic(Material):
+    """Linear Elastic Material."""
+
+    _props = ("density", "youngs_modulus")
+
+    def __init__(self, material_properties):
+        """
+        Create a Linear Elastic material.
+
+        Arguments
+        ---------
+        material_properties: dict
+            Dictionary with material properties. For linear elastic
+        materials, 'density' and 'youngs_modulus' are required keys.
+        """
+        for key in self._props:
+            if key not in material_properties:
+                raise KeyError(
+                    f"'{key}' should be present in `material_properties` "
+                    f"for LinearElastic materials."
+                )
+
+        self.material_properties = material_properties
