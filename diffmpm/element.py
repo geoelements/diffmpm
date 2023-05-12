184d2c2891ec972dfb1d1ea36f14659a645d752d
Chahak Mehta <chahakmehta013@gmail.com>
2023-05-12 17:00:45 -0500

    Add Element containers

--- diffmpm/element.py
+++ diffmpm/element.py
@@ -1,30 +1,228 @@
+import abc
+import itertools
+from typing import Sequence
+
 import jax.numpy as jnp
 
-class Bar1D:
+from diffmpm.node import Nodes
+
+
+class _Element(abc.ABC):
+    @abc.abstractmethod
+    def id_to_node_ids(self):
+        ...
+
+    @abc.abstractmethod
+    def id_to_node_loc(self):
+        ...
+
+    @abc.abstractmethod
+    def id_to_node_vel(self):
+        ...
+
+    # @abc.abstractmethod
+    # def tree_flatten(self):
+    #     ...
+
+    # @classmethod
+    # @abc.abstractmethod
+    # def tree_unflatten(self):
+    #     ...
+
+
+class Linear1D(_Element):
     """
-    1D bar element with 2 nodes.
+    Container for 1D line elements (and nodes).
 
-    Attributes:
+    Element ID:            0     1     2     3
+    Mesh:               +-----+-----+-----+-----+
+    Node IDs:           0     1     2     3     4
 
-    id: int
-        Element index.
-    
-    nid1: int
-        Node index 1.
-    
-    nid2: int
-        Node index 2.
-
-    length: float
-        Element length.
-    
-    particles: list
-        List of particles in bar.
+    + : Nodes
+    +-----+ : An element
     """
 
-    def __init__(self):
-        self.id = None
-        self.node1 = None
-        self.node2 = None
-        self.size = jnp.array([0])
-        self.particles = []
+    def __init__(self, nelements: int, el_len: float):
+        """Initialize Linear1D.
+
+        Arguments
+        ---------
+        nelements : int
+            Number of elements.
+        el_len : float
+            Length of each element.
+        """
+        self.ids: jnp.ndarray = jnp.arange(nelements)
+        self.nodes: Nodes = Nodes(
+            nelements + 1, jnp.arange(nelements + 1) * el_len
+        )
+
+    def id_to_node_ids(self, id: int):
+        """
+        Node IDs corresponding to element `id`.
+
+        Arguments
+        ---------
+        id : int
+            Element ID.
+
+        Returns
+        -------
+        jax.numpy.ndarray
+            Nodal IDs of the element. Shape of returned
+        array is (2, 1)
+        """
+        return jnp.array([id, id + 1]).reshape(2, 1)
+
+    def id_to_node_loc(self, id: int):
+        """
+        Node locations corresponding to element `id`.
+
+        Arguments
+        ---------
+        id : int
+            Element ID.
+
+        Returns
+        -------
+        jax.numpy.ndarray
+            Nodal locations for the element. Shape of returned
+        array is (2, 1)
+        """
+        return self.nodes.loc[jnp.array([id, id + 1])].reshape(2, 1)
+
+    def id_to_node_vel(self, id: int):
+        """
+        Node velocities corresponding to element `id`.
+
+        Arguments
+        ---------
+        id : int
+            Element ID.
+
+        Returns
+        -------
+        jax.numpy.ndarray
+            Nodal velocities for the element. Shape of returned
+        array is (2, 1)
+        """
+        return self.nodes.velocity[jnp.array([id, id + 1])].reshape(2, 1)
+
+
+class Quadrilateral4Node(_Element):
+    """
+    Container for 2D quadrilateral elements with 4 nodes.
+
+    Nodes and elements are numbered as
+
+                 15 0---0---0---0---0 19
+                    | 8 | 9 | 10| 11|
+                 10 0---0---0---0---0 14
+                    | 4 | 5 | 6 | 7 |
+                  5 0---0---0---0---0 9
+                    | 0 | 1 | 2 | 3 |
+                    0---0---0---0---0
+                    0   1   2   3   4
+
+    + : Nodes
+    +---+
+    |   | : An element
+    +---+
+    """
+
+    def __init__(
+        self, nelements: Sequence[int, int], el_len: Sequence[float, float]
+    ):
+        """Initialize Quadrilateral4Node.
+
+        Arguments
+        ---------
+        nelements : (int, int)
+            Number of elements in X and Y direction.
+        el_len : (float, float)
+            Length of each element in X and Y direction.
+        """
+        self.nelements: jnp.ndarray = jnp.asarray(nelements)
+        el_len = jnp.asarray(nelements)
+        total_elements = jnp.product(nelements)
+        self.ids: jnp.ndarray = jnp.arange(total_elements)
+
+        total_nodes = jnp.product(nelements + 1)
+        coords = jnp.asarray(
+            list(
+                itertools.product(
+                    jnp.arange(nelements[1] + 1), jnp.arange(nelements[0] + 1)
+                )
+            )
+        )
+        node_locations = jnp.asarray([coords[:, 1], coords[:, 0]]).T * el_len
+        self.nodes: Nodes = Nodes(total_nodes, node_locations)
+
+    def id_to_node_ids(self, id: int):
+        """
+        Node IDs corresponding to element `id`.
+
+            3----2
+            |    |
+            0----1
+
+        Node ids are returned in the order as shown in the figure.
+
+        Arguments
+        ---------
+        id : int
+            Element ID.
+
+        Returns
+        -------
+        jax.numpy.ndarray
+            Nodal IDs of the element. Shape of returned
+        array is (4, 2)
+        """
+        lower_left = (id // self.nelements[0]) * (
+            self.nelements[0] + 1
+        ) + id % self.nelements[0]
+        return jnp.asarray(
+            [
+                lower_left,
+                lower_left + 1,
+                lower_left + self.nelements[0] + 2,
+                lower_left + self.nelements[0] + 1,
+            ]
+        )
+
+    def id_to_node_loc(self, id: int):
+        """
+        Node locations corresponding to element `id`.
+
+        Arguments
+        ---------
+        id : int
+            Element ID.
+
+        Returns
+        -------
+        jax.numpy.ndarray
+            Nodal locations for the element. Shape of returned
+        array is (4, 2)
+        """
+        node_ids = self.id_to_node_ids(id)
+        return self.nodes.loc[node_ids]
+
+    def id_to_node_vel(self, id: int):
+        """
+        Node velocities corresponding to element `id`.
+
+        Arguments
+        ---------
+        id : int
+            Element ID.
+
+        Returns
+        -------
+        jax.numpy.ndarray
+            Nodal velocities for the element. Shape of returned
+        array is (4, 2)
+        """
+        node_ids = self.id_to_node_ids(id)
+        return self.nodes.vel[node_ids]
--- diffmpm/node.py
+++ diffmpm/node.py
@@ -29,9 +29,7 @@ class Nodes:
         Damping forces on the nodes.
     """
 
-    def __init__(
-        self, nnodes, position, velocity, mass, momentum, f_int, f_ext, f_damp
-    ):
+    def __init__(self, nnodes, loc):
         """
         Initialize container for Nodes.
 
@@ -39,50 +37,39 @@ class Nodes:
         ----------
         nnodes : int
             Number of nodes stored.
-        position : array_like
-            Position of all the nodes.
-        velocity : array_like
-            Velocity of all the nodes.
-        mass : array_like
-            Mass of all the nodes.
-        momentum : array_like
-            Momentum of all the nodes.
-        f_int : array_like
-            Internal forces on all the nodes.
-        f_ext : array_like
-            External forces present on all the nodes.
-        f_damp : array_like
-            Damping forces on the nodes.
+        loc : array_like
+            Locations of all the nodes.
         """
         self.nnodes = nnodes
-        self.position = position
-        self.velocity = velocity
-        self.mass = mass
-        self.momentum = momentum
-        self.f_int = f_int
-        self.f_ext = f_ext
-        self.f_damp = f_damp
+        self.loc = loc
+        self.velocity = jnp.zeros_like(self.loc)
+        self.mass = jnp.zeros_like((self.loc.shape[0], 1))
+        self.momentum = jnp.zeros_like(self.loc)
+        self.f_int = jnp.zeros_like(self.loc)
+        self.f_ext = jnp.zeros_like(self.loc)
+        self.f_damp = jnp.zeros_like(self.loc)
         return
 
-    def tree_flatten(self):
-        """Helper method for registering class as Pytree type."""
-        children = (
-            self.position,
-            self.velocity,
-            self.mass,
-            self.momentum,
-            self.f_int,
-            self.f_ext,
-            self.f_damp,
-        )
-        aux_data = (self.nnodes,)
-        return (children, aux_data)
+    # def tree_flatten(self):
+    #     """Helper method for registering class as Pytree type."""
+    #     children = (
+    #         self.position,
+    #         self.velocity,
+    #         self.mass,
+    #         self.momentum,
+    #         self.f_int,
+    #         self.f_ext,
+    #         self.f_damp,
+    #     )
+    #     aux_data = (self.nnodes,)
+    #     return (children, aux_data)
 
-    @classmethod
-    def tree_unflatten(cls, aux_data, children):
-        return cls(*aux_data, *children)
+    # @classmethod
+    # def tree_unflatten(cls, aux_data, children):
+    #     return cls(*aux_data, *children)
 
     def reset_values(self):
+        """Reset nodal parameter values except location."""
         self.velocity = self.velocity.at[:].set(0)
         self.mass = self.mass.at[:].set(0)
         self.momentum = self.momentum.at[:].set(0)
@@ -91,10 +78,13 @@ class Nodes:
         self.f_damp = self.f_damp.at[:].set(0)
 
     def __len__(self):
+        """Set length of class as number of nodes."""
         return self.nnodes
 
     def __repr__(self):
+        """Repr containing number of nodes."""
         return f"Nodes(n={self.nnodes})"
 
     def get_total_force(self):
+        """Calculate total force on the nodes."""
         return self.f_int + self.f_ext + self.f_damp
