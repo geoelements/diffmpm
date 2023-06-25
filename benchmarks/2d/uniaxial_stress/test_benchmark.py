f9fe61f0ac89fdef3b3dfca96ba7b5188d5402e8
Chahak Mehta <chahakmehta013@gmail.com>
2023-06-25 15:22:37 -0700

    Update benchmarks

--- benchmarks/2d/uniaxial_stress/test_benchmark.py
+++ benchmarks/2d/uniaxial_stress/test_benchmark.py
@@ -9,9 +9,9 @@ os.chdir(curr_dir)
 mpm = MPM("mpm-uniaxial-stress.toml")
 mpm.solve()
 
-result = jnp.load("results/uniaxial-stress-usf/particles_10.npz")
+result = jnp.load("results/uniaxial-stress-usf/particles_09.npz")
 true_stress_yy = -1.0
 true_stress_xx = 0.0
 
-assert jnp.round(result["stress"][:, 1].max() - true_stress_yy, 8) == 0.0
-assert jnp.round(result["stress"][:, 0].max() - true_stress_xx, 8) == 0.0
+assert jnp.round(result["stress"][0, :, 1].max() - true_stress_yy, 8) == 0.0
+assert jnp.round(result["stress"][0, :, 0].max() - true_stress_xx, 8) == 0.0
