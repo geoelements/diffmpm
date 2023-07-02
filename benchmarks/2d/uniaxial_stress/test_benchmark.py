4d4090ec995e59cea0a70932f221a9cc6c7e2680
Chahak Mehta <chahakmehta013@gmail.com>
2023-07-02 16:08:19 -0700

    Reformat to use with pytest

--- benchmarks/2d/uniaxial_stress/test_benchmark.py
+++ benchmarks/2d/uniaxial_stress/test_benchmark.py
@@ -3,15 +3,17 @@ from pathlib import Path
 import jax.numpy as jnp
 from diffmpm import MPM
 
-curr_filepath = Path(__file__).absolute()
-curr_dir = curr_filepath.parent
-os.chdir(curr_dir)
-mpm = MPM("mpm-uniaxial-stress.toml")
-mpm.solve()
 
-result = jnp.load("results/uniaxial-stress-usf/particles_09.npz")
-true_stress_yy = -1.0
-true_stress_xx = 0.0
+def test_benchmarks():
+    curr_filepath = Path(__file__).absolute()
+    curr_dir = curr_filepath.parent
+    os.chdir(curr_dir)
+    mpm = MPM("mpm-uniaxial-stress.toml")
+    mpm.solve()
 
-assert jnp.round(result["stress"][0, :, 1].max() - true_stress_yy, 8) == 0.0
-assert jnp.round(result["stress"][0, :, 0].max() - true_stress_xx, 8) == 0.0
+    result = jnp.load("results/uniaxial-stress-usf/particles_09.npz")
+    true_stress_yy = -1.0
+    true_stress_xx = 0.0
+
+    assert jnp.round(result["stress"][0, :, 1].max() - true_stress_yy, 8) == 0.0
+    assert jnp.round(result["stress"][0, :, 0].max() - true_stress_xx, 8) == 0.0
