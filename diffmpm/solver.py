92217c67b4983b202b248457a81b2426c46794e5
Chahak Mehta <chahakmehta013@gmail.com>
2023-07-02 15:37:59 -0700

    Move `MPM` to `__init__.py` and add CLI

--- diffmpm/solver.py
+++ diffmpm/solver.py
@@ -6,47 +6,7 @@ from jax import lax
 from jax.experimental.host_callback import id_tap
 from jax.tree_util import register_pytree_node_class
 
-import diffmpm.writers as writers
-from diffmpm.io import Config
 from diffmpm.scheme import USF, USL, _schemes
-
-
-# TODO: Move to __init__.py mostly.
-class MPM:
-    def __init__(self, filepath):
-        self._config = Config(filepath)
-        mesh = self._config.parse()
-        out_dir = Path(self._config.parsed_config["output"]["folder"]).joinpath(
-            self._config.parsed_config["meta"]["title"],
-        )
-
-        write_format = self._config.parsed_config["output"].get("format", None)
-        if write_format is None or write_format.lower() == "none":
-            writer_func = None
-        elif write_format == "npz":
-            writer_func = writers.NPZWriter().write
-        else:
-            raise ValueError(f"Specified output format not supported: {write_format}")
-
-        if self._config.parsed_config["meta"]["type"] == "MPMExplicit":
-            self.solver = MPMExplicit(
-                mesh,
-                self._config.parsed_config["meta"]["dt"],
-                velocity_update=self._config.parsed_config["meta"]["velocity_update"],
-                sim_steps=self._config.parsed_config["meta"]["nsteps"],
-                out_steps=self._config.parsed_config["output"]["step_frequency"],
-                out_dir=out_dir,
-                writer_func=writer_func,
-            )
-        else:
-            raise ValueError("Wrong type of solver specified.")
-
-    def solve(self):
-        """Solve the MPM simulation."""
-        arrays = self.solver.solve_jit(
-            self._config.parsed_config["external_loading"]["gravity"],
-        )
-        return arrays
 
 
 @register_pytree_node_class
@@ -115,6 +75,7 @@ class MPMExplicit:
 
     def solve(self, gravity: float | jnp.ndarray):
         from collections import defaultdict
+
         from tqdm import tqdm
 
         result = defaultdict(list)
