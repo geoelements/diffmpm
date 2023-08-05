d2050fbed8d6432046967234a51c6a162a5a54dc
Sachin Jalan <sachinjalan03@gmail.com>
2023-08-05 14:24:04 +0530

    support for particle_sets added

--- utitlity_functions_benchmarks.py
+++ utitlity_functions_benchmarks.py
@@ -22,6 +22,11 @@ def entity_sets(input_file_path: str, output_file_path: str):
         data = json.load(f)
     dictionary = {}
     dictionary2 = {}
+    if("particle_sets" in data):
+        for i in data["particle_sets"]:
+            dictionary2[i["id"]] = i["set"]
+        dictionary["particle_sets"] = dictionary2
+    dictionary2 = {}
     for i in data["node_sets"]:
         dictionary2[i["id"]] = i["set"]
     dictionary["node_sets"] = dictionary2
@@ -30,6 +35,19 @@ def entity_sets(input_file_path: str, output_file_path: str):
 
 
 def particles_txt_to_json(input_file_path: str, output_file_path: str):
+    """
+        Creates a json file of particles required by diff-mpm
+    code from the txt file of particles of the CB-Geo MPM code.
+
+    Parameters
+    ----------
+    input_file_path : str
+    output_file_path : str
+
+    Returns
+    -------
+    None
+    """
     if not os.path.exists(input_file_path):
         raise FileNotFoundError("File does not exist")
     with open(input_file_path, "r") as f:
